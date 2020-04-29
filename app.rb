fa6decb34a78a82ccf25b4bfb7992d71cfbdcd46
JUAN M RESTREPO <tinto922@gmail.com>
2020-04-29 18:51:44 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -42,13 +42,13 @@ class App < Sinatra::Base
       "#{@add}"
     elsif @operation == "subtract"
       @subtract = @number1 - @number2
-      @subtract
+      "#{@subtract}"
     elsif @operation == "multiply"
       @multiply = @number1 * @number2
-      @multiply
+      "#{@multiply}"
     elsif @opearation == "divide"
       @divide = @number1 / @number2
-      @divide
+      "#{@divide}"
     end 
   end 
 end
