c6fbed273eca761f2c8b498440b5306aae907565
JUAN M RESTREPO <tinto922@gmail.com>
2020-04-29 17:55:59 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ require_relative 'config/environment'
 class App < Sinatra::Base
   # Write your code here!
   get '/reversename/:name' do 
-    @name_reverse = params[:name].
+    @name_reverse = params[:name].reverse
     
   end 
 end
