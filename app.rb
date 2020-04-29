08ea14498280a0732cb3b4fc8d03912612bf36ae
JUAN M RESTREPO <tinto922@gmail.com>
2020-04-29 18:39:41 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -28,7 +28,7 @@ class App < Sinatra::Base
     @word4 = params[:word4]
     @word5 = params[:word5]
     
-    word1 + " " +
+    word1 + " " + word1 + " " + word1 + " " + word1 + " " + word5
   end 
   
   get '/:operation/:number1/:number2' do 
